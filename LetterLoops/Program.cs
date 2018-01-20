ed6e3bc20b21cf2c107e60f44ef0ef570d1506d9
Gina <ginamantonini@gmail.com>
2018-01-19 22:46:09 -0600

    stepping through exercise

--- LetterLoops/Program.cs
+++ LetterLoops/Program.cs
@@ -10,6 +10,28 @@ namespace LetterLoops
     {
         static void Main(string[] args)
         {
+            //first count how many characters are in the input
+            string input = "abcd";
+
+            //next loop over the input and print out 1st character once (uppercase), insert a dash, then, repeat
+            for (int i = 0; i < input.Count(); i++)
+            {
+                var onecharacter = input[i];
+                //if the index is 0, change the letter to uppercase
+                //for all other characters in the input, print and repeat by one more 
+                if (i == 0)
+                {
+                    onecharacter.ToUpper;
+                }
+                    
+            }
+
+               
+                   
         }
+
     }
+
+
 }
+
